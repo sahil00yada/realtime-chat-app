64b130194f8f6f4435939b660b9085173b90d100
sahil00yada <166053242+sahil00yada@users.noreply.github.com>
2026-04-19 19:30:33 +0530

    Comment out user retrieval in login function
    Commented out the user retrieval line in the login function.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -50,7 +50,7 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email });
+    // const user = await User.findOne({ email });
 
     if (!user) {
       return res.status(400).json({ message: "Invalid credentials" });
